64c86a6a087855e9fcf9d0ff5ed5bdb1302717dc
jovidalao <jovidalao@gmail.com>
2025-08-19 00:35:18 +1000

    04: background job

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,28 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
-  createAI: baseProcedure
-    .input(
-      z.object({
+    invoke: baseProcedure.input(z.object({
         text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+    })).mutation(async ({ input }) => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                email: input.text,
+            }
+        })
     }),
+    createAI: baseProcedure
+        .input(
+            z.object({
+                text: z.string(),
+            }),
+        )
+        .query((opts) => {
+            return {
+                greeting: `hello ${opts.input.text}`,
+            };
+        }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
