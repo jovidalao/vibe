94d4618cf790e91f91e8e274ae9505e6009d3818
jovidalao <jovidalao@gmail.com>
2025-09-07 11:36:39 +1000

    12: fragment preview

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -9,6 +9,7 @@ import { MessageContainer } from "../components/messages-container";
 import { Suspense, useState } from "react";
 import { Fragment } from "@/generated/prisma";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 interface Props {
     projectId: string;
@@ -43,7 +44,7 @@ export const ProjectView = ({ projectId }: Props) => {
                     minSize={50}
                     className="flex flex-col min-h-0"
                 >
-                    TODO: Preview panel
+                    {activeFragment && <FragmentWeb data={activeFragment} />}
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
