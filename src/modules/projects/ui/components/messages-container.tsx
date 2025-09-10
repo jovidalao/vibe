1b4d7134fad862610f8556adc09664aad0088ecd
jovidalao <jovidalao@gmail.com>
2025-09-10 22:27:17 +1000

    14: fix

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -29,10 +29,13 @@ export const MessageContainer = ({ projectId, activeFragment, setActiveFragment 
 
     useEffect(() => {
         const lastAssistantMessage = messages.findLast(
-            (message) => message.role === "ASSISTANT" && message.fragment
+            (message) => message.role === "ASSISTANT"
         );
-        if (lastAssistantMessage) {
+        if (
+            lastAssistantMessage?.fragment && lastAssistantMessage.id !== lastAssistantMessageRef.current
+        ) {
             setActiveFragment(lastAssistantMessage.fragment);
+            lastAssistantMessageRef.current = lastAssistantMessage.id;
         }
     }, [messages, setActiveFragment])
 
@@ -48,25 +51,25 @@ export const MessageContainer = ({ projectId, activeFragment, setActiveFragment 
             <div className="flex-1 min-h-0 overflow-y-auto">
                 <div className="pt-2 pr-1 pb-2">
                     {messages.map((message) => (
-                        <MessageCard 
-                        key={message.id}
-                        content={message.content}
-                        role={message.role}
-                        fragment={message.fragment}
-                        createdAt={message.createdAt}
-                        isActiveFragment={activeFragment?.id === message.fragment?.id}
-                        onFragmentClick={() => setActiveFragment(message.fragment)}
-                        type={message.type}
-                    />  
+                        <MessageCard
+                            key={message.id}
+                            content={message.content}
+                            role={message.role}
+                            fragment={message.fragment}
+                            createdAt={message.createdAt}
+                            isActiveFragment={activeFragment?.id === message.fragment?.id}
+                            onFragmentClick={() => setActiveFragment(message.fragment)}
+                            type={message.type}
+                        />
                     ))}
                     {isLastMessageUser && <MessageLoading />}
                     <div ref={bottomRef} />
                 </div>
             </div>
             <div className="relative p-3 pt-1">
-                <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background pointer-events-none"/>
+                <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background pointer-events-none" />
                 <MessageForm projectId={projectId} />
-            </div> 
+            </div>
         </div>
     )
 }
